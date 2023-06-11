19a293e11d68c9dc85a223f83b410cf8d848cce3
Mohammad Habibi <mdhi2000@gmail.com>
2023-06-11 12:43:35 +0330

    added flask

--- main.py
+++ main.py
@@ -1,12 +1,35 @@
-import urllib.request
-import json
+from flask import Flask
+from flask import request
+from flask import jsonify
+import requests
+import subprocess
+
+app = Flask(__name__)
+
+@app.route('/image/create', methods=['GET'])
+def create_image():
+  prompt = request.args.get('p')
+  output = subprocess.run(['ls', '-l'])
+  print(output)
+  # cookies = {}
+  # requests.get(f"https://www.bing.com/images/create?q={prompt}&rt=4&FORM=GENCRE", cookies=cookies)
+  
+  # return render(request, '')
 
 def main():
-  with open("./processed_songs_1000.local.json") as f:
-    songs = json.load(f)
-  
-  print(songs[0])
-  urllib.request.urlretrieve(songs[0]["hq_link"], "temp.mp3")
+  app.run(port=8001)
 
 if __name__ == '__main__':
+  output = subprocess.run([
+      'python',
+      '-m',
+      'BingImageCreator',
+      '-U',
+      '19TUgTuQeLpOGAhqHwUtzq-YUrRnIh2qgxvJMgvrkA6Qoe7a0H38-mC2iCclafjJ-g4Oqk22qdRGjUME8PQAPBrgZh9tGdGohQzbAWurho75vGegz_cfhw2avJJ3hm7gMR8fRBqSusG96NMNC692eA-sN_RgBCAmDHZuOEjhBDecd5SmVLzq0kgoycHUXdanw6TTsWlB0xUNaO2VGBDX2svkCCiAudnd45LSDozAapE8',
+      '--prompt',
+      'کاور آلبوم آهنگ با موضوع بهار، نوروز،نوروز ۱۴۰۲',
+      '--output-dir',
+      './output'
+    ])
+  print(output)
   main()
